a061b87c490107c88337f4074e0cc7fa1e8e8561
SKY-20171204AVE\Administrator <1160596695@qq.com>
2018-04-18 17:31:02 +0800

    添加订单页面20180408

--- wedhlj/js/ub/u_AddWeddingOrder_Details.js
+++ wedhlj/js/ub/u_AddWeddingOrder_Details.js
@@ -9,7 +9,6 @@ $(document).ready(function(){
 	//_content 选中框内容填放处标记
 	function dropDown(_item,_content){
 		$(_item).click(function(){
-		console.log($(this).html());
 		$(_content).html($(this).html())
 	})
 	}
@@ -21,6 +20,48 @@ $(document).ready(function(){
 	dropDown('.hotelAddrContent a','.hotelAddr')
 	//接亲地址
 	dropDown('.jieqinAddrContent a','.jieqinAddr')
-	
+	// *添加订单详情的区域
+	dropDown('.addOrderItem_AddrContent a','.addOrderItem_Addr')
+	// *添加订单详情的名称
+	dropDown('.addOrderItem_NameContent a','.addOrderItem_Name')
+	// *添加订单详情的性质
+	dropDown('.addOrderItem_PriContent a','.addOrderItem_Pri')
+	// *添加人员详情的人员类型
+	dropDown('.addPersonItem_TypeContent a','.addPersonItem_Type')
+
+
+	// 点击订单详情的XX
+	$(".addOrderItemTitle span").click(function(){
+		$(".addOrderItems").css("display","none");
+	})
+	// 点击订单详情的添加按钮
+	$(".orderAddButton").click(function(){
+		$(".addOrderItems").css("display","block");
+	})
+	/*人员详情*/
+	// 点击人员详情的XX
+	$(".addPersonItemTitle span").click(function(){
+		$(".addPersonItems").css("display","none");
+	})
+	// 点击人员详情的添加按钮
+	$(".personAddButton").click(function(){
+		$(".addPersonItems").css("display","block");
+	})
+	$(function(){
+		$(".check").click(function(){
+			$(":input[name='ids']").prop("checked",this.checked);
+		});
+		$(":checkbox[name='ids']").click(function(){
+			$(".check").prop("checked",$(":checkbox[name='ids']").length==$(":checkbox[name='ids']:checked").length);
+		});
+	})
+	$(function(){
+		$(".ordercheck").click(function(){
+			$(":input[name='cds']").prop("checked",this.checked);
+		});
+		$(":checkbox[name='cds']").click(function(){
+			$(".check").prop("checked",$(":checkbox[name='cds']").length==$(":checkbox[name='cds']:checked").length);
+		});
+	})
 })
 
