1e4a1e6af4089192bd1f0723fd86efa0298f41c8
SKY-20171016OZZ\Administrator <1160596695@qq.com>
2018-04-19 11:09:35 +0800

    固定人员上传静态页面

---
+++ wedhlj/js/ub/u_AddCollaborator.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	//导航栏默认选中
+ 	$(".nav_cont_a").eq(1).addClass("nav_cont_on");
+})
+//选风格,最多只能添加三种风格
+var addStyle=1;//是否可以添加样式
+$(".Posttask_x10 li").click(function(){
+	var choosedLi=$(".Posttask_x20_on").length+1;
+	 if(choosedLi>3){
+	 	if($(this).hasClass("Posttask_x20_on")){
+	 		$(this).toggleClass(".Posttask_x20_on");
+	 		addStyle=1;
+	 	}else{
+	 		addStyle=0;
+	 	}
+	 }
+	 if(addStyle==1){	
+		 $(this).toggleClass("Posttask_x20_on");
+	}
+})
