ea51cb9ece504f15e801384824d92347eaaaa688
SKY-20171204AVE\Administrator <1160596695@qq.com>
2018-04-26 14:35:05 +0800

    策划编辑

--- wedhlj/js/ub/u_PlanEditCommodity.js
+++ wedhlj/js/ub/u_PlanEditCommodity.js
@@ -1,5 +1,4 @@
 var schemeNo=getUrlParam('schemeNo');
-console.log(schemeNo);
 queryScheme(schemeNo);
 //价格必须为正数
 $(".input_money").change(function() {
@@ -45,13 +44,23 @@ $(".Upload").click(function(){
 			return false;
 		}else if(imgFile){
 			for(var p=0;p<imgFile.length;p++){
-				if(imgFile[p].length <= 0){
+				if(imgbox_default[p].length+imgFile[p].length <= 0){
 					meg("提示","上传图片不能为空","body");//限制上传个数
 					return false;
 				}
 			}
 		}
-		on_Loading()	
+		on_Loading()
+		//修改图片
+		for(var r=0;r<imgbox_name.length;r++){
+			var img_hide = "";
+			for(var t=0;t<imgbox_name[r].length;t++){
+				if(imgbox_name[r][t]){
+					img_hide += imgbox_name[r][t]+",";
+				}	
+			}
+			$(".img_hide input").eq(r).val(img_hide);
+		}	
 		//用formDate对象上传
 		var data = new FormData($('#uploadForm')[0]);
 		for(var i=0;i<imgFile.length;i++){
@@ -63,20 +72,20 @@ $(".Upload").click(function(){
 		function dothing(){
 			window.location.href = "u_PlanManagement.html"
 		}
-		data.append("token",$.cookie('login_on'));
+		data.append("schemeNo",schemeNo);
+		///scheme/updateScheme
 		$.ajax({
 			type: "post",
-			url: apiUrl+"/scheme/addSchemeInfo",
+			url: apiUrl+"/scheme/updateScheme",
 			data: data,
 			processData: false,
 			contentType: false,
 			success: function(e) {
 				down_Loading()
-				console.log(e);
 				if (e.status == "200") {
-					meg("提示","策划上传成功","body",dothing);
+					meg("提示","策划修改成功","body",dothing);
 				}else{
-					meg("提示","策划上传失败","body");
+					meg("提示","策划修改失败","body");
 				}
 			},
 			error : function(e) {
@@ -90,8 +99,6 @@ $(".Upload").click(function(){
 function on_navli(){
 	$(".nav_cont_a").eq(3).addClass("nav_cont_on");
 }
-
-
 //获取url中的参数
 function getUrlParam(name){
 	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
@@ -107,24 +114,116 @@ function queryScheme(schemeNo){
 			data: {schemeNo:schemeNo},
 			success: function(e) {
 				down_Loading()
-				console.log(e.scheme);
 				var scheme=e.scheme[0];
-				console.log(scheme.schemeName);
+				console.log(scheme);
 				// 案例名称
 				$("input[name=schemeName]").val(scheme.schemeName);
 				// 案例风格
-				 //$("input[name=schemeStyle]").val(scheme.schemeStyle);
+				 for(var i=0;i<$(".input_style_cont li>p").length;i++){
+				 	if($(".input_style_cont li>p")[i].innerHTML==scheme.schemeStyle){
+				 		$(".input_style_cont li")[i].setAttribute('class','input_style_x10');
+				 	}
+				 }
 				// 策划师
 				$("input[name=schemeDesigner]").val(scheme.schemeDesigner);
 				// 价格
 				$("input[name=schemePrice]").val(scheme.schemePrice)
 				// 案例介绍
+				$("textarea[name=schemeDesc]").html(scheme.schemeDesc)
 				// 设计思路
+				$("textarea[name=schemeDesign]").html(scheme.schemeDesign)
 				// 主舞台
+				var sStage_img = new Array();
+				sStage_img = (scheme.schemeStageArea).split(",");
+				if(sStage_img.length >= 5){
+					$(".upload_img").eq(0).find(".upload_img_Choice").css("display","none");
+				}
+				var sStage = "";
+				for(var a=0;a<sStage_img.length;a++){
+					imgbox_img[0].push(sStage_img[a])
+					sStage +='<div class="main_file">'+
+						'<div class="show">'+
+						'<img src="'+apiUrl+sStage_img[a]+'" alt="">'+
+						'</div>'+
+						'<div onclick="remove_default(this,\'0\')" class="main_file_hide remove_default">删除</div>'+
+						'</div>';
+				}
+				$("#imgBox01").html(sStage);
 				// 礼道区
+				var sPassage_img = new Array();
+				sPassage_img = (scheme.schemePassageArea).split(",");
+				if(sPassage_img.length >= 5){
+					$(".upload_img").eq(1).find(".upload_img_Choice").css("display","none");
+				}
+				var sPassage = "";
+				for(var a=0;a<sPassage_img.length;a++){
+					imgbox_img[1].push(sPassage_img[a])
+					sPassage +='<div class="main_file">'+
+						'<div class="show">'+
+						'<img src="'+apiUrl+sPassage_img[a]+'" alt="">'+
+						'</div>'+
+						'<div onclick="remove_default(this,\'1\')" class="main_file_hide remove_default">删除</div>'+
+						'</div>';
+				}
+				$("#imgBox02").html(sPassage);
 				// 签到区
+				var sSign_img = new Array();
+				sSign_img = (scheme.schemeSignArea).split(",");
+				if(sSign_img.length >= 5){
+					$(".upload_img").eq(2).find(".upload_img_Choice").css("display","none");
+				}
+				var sSign = "";
+				for(var a=0;a<sSign_img.length;a++){
+					imgbox_img[2].push(sSign_img[a])
+					sSign +='<div class="main_file">'+
+						'<div class="show">'+
+						'<img src="'+apiUrl+sSign_img[a]+'" alt="">'+
+						'</div>'+
+						'<div onclick="remove_default(this,\'2\')" class="main_file_hide remove_default">删除</div>'+
+						'</div>';
+				}
+				$("#imgBox03").html(sSign);
 				// 合影区
+				var sPhoto_img = new Array();
+				sPhoto_img = (scheme.schemePhotoArea).split(",");
+				if(sPhoto_img.length >= 5){
+					$(".upload_img").eq(3).find(".upload_img_Choice").css("display","none");
+				}
+				var sPhoto = "";
+				for(var a=0;a<sPhoto_img.length;a++){
+					imgbox_img[3].push(sPhoto_img[a])
+					sPhoto +='<div class="main_file">'+
+						'<div class="show">'+
+						'<img src="'+apiUrl+sPhoto_img[a]+'" alt="">'+
+						'</div>'+
+						'<div onclick="remove_default(this,\'3\')" class="main_file_hide remove_default">删除</div>'+
+						'</div>';
+				}
+				$("#imgBox04").html(sPhoto);
 				// 甜品区
+				var sSweetmeats_img = new Array();
+				sSweetmeats_img = (scheme.schemeSweetmeatsArea).split(",");
+				if(sSweetmeats_img.length >= 5){
+					$(".upload_img").eq(4).find(".upload_img_Choice").css("display","none");
+				}
+				var sSweetmeats = "";
+				for(var a=0;a<sSweetmeats_img.length;a++){
+					imgbox_img[4].push(sSweetmeats_img[a])
+					sSweetmeats +='<div class="main_file">'+
+						'<div class="show">'+
+						'<img src="'+apiUrl+sSweetmeats_img[a]+'" alt="">'+
+						'</div>'+
+						'<div onclick="remove_default(this,\'4\')" class="main_file_hide remove_default">删除</div>'+
+						'</div>';
+				}
+				$("#imgBox05").html(sSweetmeats);
+				//获取需要修改的图片，存入数组
+				for(var j=0;j<$(".upload_img").length;j++){
+					var this_box = $(".upload_img").eq(j).find(".main_file");
+					for(var k=0;k<this_box.length;k++){
+						imgbox_default[j].push('<div class="main_file">'+this_box.eq(k).html()+'</div>')
+					}
+				}
 			},
 			error : function(e) {
 				down_Loading()
