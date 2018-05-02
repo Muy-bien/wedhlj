f6c6e144ccf9fca430e2ebc48c45a950233447fb
SKY-20171016OZZ\Administrator <1160596695@qq.com>
2018-05-02 17:29:00 +0800

    服务商超市展示页

--- wedhlj/js/ub/u_management_edit.js
+++ wedhlj/js/ub/u_management_edit.js
@@ -1,12 +1,5 @@
 //导入数据
 $(document).ready(function(){
-	//获取url中的参数
-	function getUrlParam(name){
-		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-		//构造一个含有目标参数的正则表达式对象
-		var r = window.location.search.substr(1).match(reg);//匹配目标参数
-		if (r != null) return unescape(r[2]); return null; //返回参数值
-	}
 	//接收URL中的参数spid
 	var id = getUrlParam('id');
 	//获取商品数据渲染页面
