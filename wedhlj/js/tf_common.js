f00fa7544ac5e6c82e35335f08cb90e2ed0ecc02
SKY-20171016OZZ\Administrator <1160596695@qq.com>
2018-05-22 15:04:05 +0800

    公共样式

--- wedhlj/js/tf_common.js
+++ wedhlj/js/tf_common.js
@@ -1,7 +1,7 @@
 //动态添加头部，导航栏，底部
 $(document).ready(function(){
 	// 返回顶部按钮
-	$("body").append('<div id="icon"><a href=".box"><img src="images/icon.png" alt=""></a></div>')
+	$("body").append('<div id="icon"><a href=".box"><img src="images/icon.png" alt=""></a></div>');
 	/*头部*/
 	var header = '<div class="logo"><img src="images/logo.png" alt=""></div>'+
 		'<div class="header_cont">'+
@@ -30,7 +30,6 @@ $(document).ready(function(){
 			'</div>'+
 		'</div>';
 	$("header").html(header);
-
 	/*底部*/
 	var footer = '<div class="footer">'+
 		'<div class="footer_left">'+
@@ -53,89 +52,7 @@ $(document).ready(function(){
 		'</div>'+
 	'</div>';
 	$("footer").html(footer);
-
-	//验证当前页面是否登录
-	if($.cookie("login_on")){
-		//获取最新用户状态
-		var h_position = "";
-		var h_checkStatus = "";
-		$.ajax({
-			type: 'POST',
-			url: apiUrl+'/user/resultUserType',
-			dataType: 'json',
-			data: {userName:$.cookie("user"),token:$.cookie("login_on")},
-			success: function(e){
-				h_position = e.userType;//用户类型定位信息
-				h_checkStatus = e.auditStatus;//审核状态
-				$.cookie("position",h_position,{ path:'/',secure:false}); //储存状态
-				//商家入驻内容
-				if (!h_position || h_position == "" || h_position == 0) {
-					$(".header_ht").text('商家入驻');
-				}else if(h_position == 1 || h_position == 2 || h_position == 3){
-					$(".header_ht").text('商家后台');
-				}else if(h_position == 4){
-					$(".header_ht").text('管理员后台');
-				}
-			}
-		})
-
-
-		$(".header_user_img").addClass('header_user_img_on');
-		$(".header_ht").addClass('hp_block');
-		$(".header_user_cont").html('<span class="hp_block">'+$.cookie("user")+'</span>|<span class="off_login">退出登录</span>');//头部样式
-		
-		//登录状态点击进入后台||用户名
-		$(".hp_block").click(function(){
-			if(h_checkStatus == 1){
-				if(h_position == 4){
-					window.location.href = "c_mainCheck.html";
-				}else if(h_position == 1||h_position == 2||h_position == 3){
-					window.location.href = "u_PersonalCenter.html";
-				}
-			}else if(h_checkStatus == "-1"){
-				if(h_position == 0){
-					var str = "";
-					str = '<div id="h_position"><div class="hp_cont"><div class="hp_delete">'+
-					'<span><img src="images/hp_delete.png" alt=""></span></div><ul>'+
-					'<li class="hp_cont_x10 hpc_01"><a href="u_uploadinfo.html?info=1"><h1>您的定位:</h1><p>婚礼策划</p></a></li>'+
-					'<li class="hp_cont_x10 hpc_02"><a href="u_uploadinfo.html?info=2"><h1>您的定位:</h1><p>婚庆人员</p></a></li>'+
-					'<li class="hp_cont_x10 hpc_03"><a href="u_uploadinfo.html?info=3"><h1>您的定位:</h1><p>道具舞美</p></a></li>'+
-					'</ul></div></div>'
-					$("body").append(str);
-
-					//点击退出商户类型选择
-					$(".hp_delete span").click(function(){
-						$("#h_position").remove();
-					})
-				}else{
-					window.location.href = "b_Audit.html";
-				}
-			}else if(h_checkStatus == 0){
-				window.location.href = "b_AuditNotThrough.html";
-			}
-		})
-	}else if($.cookie("login_on") == "" || !$.cookie("login_on")){
-		var PreviousUrl = window.location.pathname + window.location.search + window.location.hash;
-		$.cookie("PreviousUrl",PreviousUrl.split("/")[1],{ path:'/',secure:false});
-		$(".header_ht").removeClass('hp_block');
-		$(".header_user_img").removeClass('header_user_img_on');
-		$(".header_user_cont").html('<a href="login.html">登录</a><span>|</span><a href="register.html">注册</a>');//头部样式
-		$(".header_ht").click(function(){
-			window.location.href = "login.html";
-		})
-	}
-
-	//点击退出登录
-	$(".off_login").click(function(){
-		meg2("提示","是否确定退出登录","body",doThing)
-		function doThing(){
-			$.cookie("login_on","",{ path:'/',secure:false , expires: -1});//清空token
-			$.cookie("user","",{ path:'/',secure:false , expires: -1});//清空用户名
-			$.cookie("position","",{ path:'/',secure:false , expires: -1});//清空用户类型
-			window.location.href = "index.html";
-		}	
-	})	
-
+	onSignIn();//驗證是否登錄
 	//返回顶部
 	$(window).scroll(function(){
 		var window_s = $(window).scrollTop();
@@ -145,31 +62,141 @@ $(document).ready(function(){
 		}else if(window_s < window_h){
 			$("#icon").css("display","none");
 		}
-	})
+	});
 	$("#icon a").click(function () {
 	    $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top}, 500);
 	    return false;//不要这句会有点卡顿
 	});
-})
-
-			
-				
-				
-					
-						
-						
-					
-					
-						
-						
-					
-					
-						
-						
-					
-				
-				
-					
-				
-			
-		
+});
+//驗證是否登錄
+function onSignIn(){
+	//验证当前页面是否登录
+	if($.cookie("login_on")){
+		//已登录
+		judgeLogin=1;
+		//获取最新用户状态
+		var h_position = "";
+		var h_checkStatus = "";
+		on_Loading();
+		$.ajax({
+			type: 'POST',
+			url: apiUrl+'/user/resultUserType',
+			dataType: 'json',
+			data: {userName:$.cookie("user"),token:$.cookie("login_on")},
+			success: function(e){
+				down_Loading();
+				h_position = e.userType;//用户类型定位信息
+				h_checkStatus = e.auditStatus;//审核状态
+				$.cookie("position",h_position,{ path:'/',secure:false}); //储存用户类型定位信息
+				$.cookie("checkStatus",h_checkStatus,{ path:'/',secure:false}); //审核状态
+				//商家入驻内容
+				if (!h_position || h_position == "" || h_position == 0) {
+					$(".header_ht").text('商家入驻');
+				}else if(h_position == 1 || h_position == 2 || h_position == 3){
+					$(".header_ht").text('商家后台');
+				}else if(h_position == 4){
+					$(".header_ht").text('管理员后台');
+				}
+			},
+			error:function(){
+				down_Loading();
+			}
+		});
+		$(".header_user_img").addClass('header_user_img_on');
+		$(".header_ht").addClass('hp_block');
+		$(".header_user_cont").html('<span class="hp_block">'+$.cookie("user")+'</span>|<span class="off_login">退出登录</span>');//头部样式
+		clickBlock(h_position,h_checkStatus);//点击进入后台触发事件
+	}else if($.cookie("login_on") == "" || !$.cookie("login_on")){
+		//未登录
+		judgeLogin=2;
+		var PreviousUrl = window.location.pathname + window.location.search + window.location.hash;
+		$.cookie("PreviousUrl",PreviousUrl.split("/")[1],{ path:'/',secure:false});
+		$(".header_ht").removeClass('hp_block');
+		$(".header_user_img").removeClass('header_user_img_on');
+		$(".header_user_cont").html('<a href="login.html">登录</a><span>|</span><a href="register.html">注册</a>');//头部样式
+		$(".header_ht").click(function(){
+			window.location.href = "login.html";
+		});
+	}
+}
+//登录状态点击进入后台||用户名
+function clickBlock(h_position,h_checkStatus){
+	$(".hp_block").click(function(){
+		if(h_checkStatus == 1){
+			if(h_position == 4){
+				window.location.href = "c_mainCheck.html";
+			}else if(h_position == 1||h_position == 2||h_position == 3){
+				window.location.href = "u_PersonalCenter.html";
+			}
+		}else if(h_checkStatus == "-1"){
+			if(h_position == 0){
+				var str = "";
+				str = '<div id="h_position"><div class="hp_cont"><div class="hp_delete">'+
+				'<span><img src="images/hp_delete.png" alt=""></span></div><ul>'+
+				'<li class="hp_cont_x10 hpc_01"><a href="u_uploadinfo.html?info=1"><h1>您的定位:</h1><p>婚礼策划</p></a></li>'+
+				'<li class="hp_cont_x10 hpc_02"><a href="u_uploadinfo.html?info=2"><h1>您的定位:</h1><p>婚庆人员</p></a></li>'+
+				'<li class="hp_cont_x10 hpc_03"><a href="u_uploadinfo.html?info=3"><h1>您的定位:</h1><p>道具舞美</p></a></li>'+
+				'</ul></div></div>';
+				$("body").append(str);
+				//点击退出商户类型选择
+				$(".hp_delete span").click(function(){
+					$("#h_position").remove();
+				});
+			}else{
+				window.location.href = "b_Audit.html";
+			}
+		}else if(h_checkStatus == 0){
+			window.location.href = "b_AuditNotThrough.html";
+		}
+	});
+	ExitLogon();//点击退出登录
+}
+//点击退出登录
+function ExitLogon(){
+	$(".off_login").click(function(){
+		meg2("提示","是否确定退出登录","body",doThing);
+		function doThing(){
+			$.cookie("login_on","",{ path:'/',secure:false , expires: -1});//清空token
+			$.cookie("user","",{ path:'/',secure:false , expires: -1});//清空用户名
+			$.cookie("position","",{ path:'/',secure:false , expires: -1});//清空用户类型
+			window.location.href = "index.html";
+		}
+	});
+}
+//驗證是否登錄
+var judgeLogin="";
+setInterval("settime()",1000);//每秒验证是否为登录
+function settime(){
+	if(judgeLogin==1){
+		if($.cookie("login_on") == "" || !$.cookie("login_on")){
+			judgeLogin=2;
+			var PreviousUrl = window.location.pathname + window.location.search + window.location.hash;
+			$.cookie("PreviousUrl",PreviousUrl.split("/")[1],{ path:'/',secure:false});
+			$(".header_ht").text('商家入驻');
+			$(".header_ht").removeClass('hp_block');
+			$(".header_user_img").removeClass('header_user_img_on');
+			$(".header_user_cont").html('<a href="login.html">登录</a><span>|</span><a href="register.html">注册</a>');//头部样式
+			$(".header_ht").click(function(){
+				window.location.href = "login.html";
+			});
+		}
+	}else if(judgeLogin==2){
+		if($.cookie("login_on")){
+			judgeLogin=1;
+			$(".header_user_img").addClass('header_user_img_on');
+			$(".header_ht").addClass('hp_block');
+			$(".header_user_cont").html('<span class="hp_block">'+$.cookie("user")+'</span>|<span class="off_login">退出登录</span>');//头部样式
+			var h_position = $.cookie("position");//用户类型定位信息
+			var h_checkStatus = $.cookie("checkStatus");//审核状态
+			//商家入驻内容
+			if (!h_position || h_position == "" || h_position == 0) {
+				$(".header_ht").text('商家入驻');
+			}else if(h_position == 1 || h_position == 2 || h_position == 3){
+				$(".header_ht").text('商家后台');
+			}else if(h_position == 4){
+				$(".header_ht").text('管理员后台');
+			}
+			clickBlock(h_position,h_checkStatus);//点击进入后台触发事件
+		}
+	}
+}
